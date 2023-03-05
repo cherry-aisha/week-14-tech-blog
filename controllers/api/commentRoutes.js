8390b7ca0b66c1f228c481fe9baff667a899099d
cherry-aisha <113786104+cherry-aisha@users.noreply.github.com>
2023-03-05 20:19:35 +0000

    Updated comment routes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,37 +2,6 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-// get all comments
-router.get('/', withAuth, async (req, res) => {
-  // find all comments
-  // be sure to include its associated Id and Data
-  try {
-    const commentData = await Comment.findAll({
-  });
-    res.status(200).json(commentData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// get one comment
-router.get('/:id', withAuth, async (req, res) => {
-  // find a single comment by its `id`
-  try {
-    const commentData = await Comment.findByPk(req.params.id, {
-    });
-
-    if (!commentData) {
-      res.status(404).json({ message: 'No comment found with this id!' });
-      return;
-    }
-
-    res.status(200).json(commentData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 // Create Comment
 router.post('/', withAuth, async (req, res) => {
   try {
@@ -62,25 +31,6 @@ router.put("/:id", withAuth, async (req, res) => {
     if (!commentData)
       res.status(404).json({ message: "No comment identified with this id!" });
         res.status(200).json(commentData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// Deletes Comments
-router.delete("/:id", withAuth, async (req, res) => {
-  try {
-    const commentData = await Comment.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
-    if (!commentData)
-      res
-        .status(404)
-        .json({ message: "No comment identified with this id" });
-
-    res.status(200).json(commentData);
   } catch (err) {
     res.status(500).json(err);
   }
